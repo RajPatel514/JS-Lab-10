77cd9a87451954eac704e64d2dd2dcc27a5ff01d
Raj Patel <raj.patel514@gmail.com>
2024-12-11 21:37:38 -0500

    Created first part of XHR function

--- script.js
+++ script.js
@@ -36,3 +36,23 @@ function displayData(data) {
     tableBody.appendChild(row);
 
 }
+
+//Task 2 - api interaction using XMLHTTPrequest - XHR
+document.getElementById('fetchxhr').addEventListener('click', function () {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/2', true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                console.log(data);
+                displayData(data);
+            } else {
+                console.error('Error fetching data:', xhr.statusText);
+            }
+        }
+    };
+
+    xhr.send();
+});
